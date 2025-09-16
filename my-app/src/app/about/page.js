a595fc67fe5e96cb550f373ab8d0055448b282aa
mustafarizwan-ops <mustafa.rizwan@codingcops.org>
2025-09-16 18:54:51 +0500

    about page

---
+++ my-app/src/app/about/page.js
@@ -0,0 +1,9 @@
+
+export default function About() {
+
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+        <h1 className="text-4xl font-bold text-center">Welcome To About Page</h1>
+    </div>
+  );
+}
